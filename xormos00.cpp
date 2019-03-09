dd775857847dac38b3e51d977fb3474419c85346
miormos <mi.ormos@gmail.com>
2019-03-09 10:37:19 +0100

    working on leaves level for 16 items well

--- xormos00.cpp
+++ xormos00.cpp
@@ -12,10 +12,10 @@ PRL Project 1
 #include <string>
 #include <fstream>
 #include <time.h>
-using namespace std; 
-
 #include <algorithm>
 #include <iterator>
+using namespace std; 
+
 void display_vector(const vector<int> &v)
 {
     copy(v.begin(), v.end(),
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
     int lastProcess;
 
     int MSG_SIZE;
-
+	const int EOS=-1;
 	// int arr_size; // = sizeof(arr)/sizeof(int);
 	// double items_of_array; // = log2(arr_size);
 	// int list_cpu;// ; = nextPowerOfTwo(items_of_array);
@@ -63,7 +63,8 @@ int main(int argc, char *argv[]) {
 	  //       cout << +character;    	
 	  //   }
     	//reading numbers from file
-    	int num, i=0;
+    	int num, numberOfItems=0;
+    	int c;
     	string fileName = "numbers";
     	vector<int> numbers;
         ifstream inputFile;
@@ -74,29 +75,75 @@ int main(int argc, char *argv[]) {
        		if(num != -1) {
        			cout << num << " ";
        			numbers.push_back(num);
-       			i++;
+       			numberOfItems++;
        		}
         }  
         // print count of numbers to stdout
-        cout << "\nNumber of numbers: " << i << "\n"; 
+        cout << "\nNumber of numbers: " << numberOfItems << "\n"; 
         cout << "Number of processes: " << processesCount << "\n";	
         // get bucket number
-        bucketSize = round(i/((processesCount+1)/2));
+        bucketSize = round(numberOfItems/((processesCount+1)/2));
         cout << "Bucket size: " << bucketSize << "\n";
+        cout << "Log2 number: " << (processesCount+1)/2 << "\n";
         display_vector(numbers);
         // MSG_SIZE = 
         number = -1;
         // send data to cpu
-        for(int i = 1; i < processesCount; i++) {
-        	MPI_Send(&numbers[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            // MPI_Send(&number, 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
-            // MPI_Send(&number, 1, MPI_INT, 1, 2, MPI_COMM_WORLD);
-        }        
+
+// for j = 1 to log m do
+	// for all processors at level (log m)-j do in parallel
+		// procesor spojí posloupnosti svých syn
+	// endfor
+// endfor
+
+// posli 4 cisla za sebou do prveho
+// tak dalsie 4 cisla v poradi do druheho
+// atd.
+
+        // for(int j = 1; j <= (processesCount+1)/2; j++) { // from 1 to 3
+	       //  	for(int z = 0; z <= j*bucketSize; z++) {
+	       //  	MPI_Send(&numbers[j], 1, MPI_INT, j+1, 0, MPI_COMM_WORLD);
+
+	       //      // MPI_Send(&number, 1, MPI_INT, i + 1, 1, MPI_COMM_WORLD);
+	       //      // MPI_Send(&number, 1, MPI_INT, 1, 2, MPI_COMM_WORLD);
+		      //   c=EOS;
+		      //   MPI_Send(&c, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);	            
+        // 	}
+        // }        
+        int proc = 1;
+        for (int j = 0; j < numberOfItems; j++) {
+        	MPI_Send(&numbers[j], 1, MPI_INT, proc, 0, MPI_COMM_WORLD);
+        	if (proc == 4) {
+        		proc = 1;
+        	} else {
+        		proc++;
+        	}
+        }
+        c=EOS;
+        for (int z = 1; z <=4; z++) {
+	        MPI_Send(&c, 1, MPI_INT, z, 0, MPI_COMM_WORLD);	 
+        }
+
     } else {
     	//SLAVES
+    	vector<int> numbersBucketSlave;
+    	bool next = true;
         int recievedData;
-	    MPI_Recv(&recievedData, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	    printf("Process received number %d from process 0\n", recievedData);    	
+        while(next) {
+		    MPI_Recv(&recievedData, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		    printf("Process %d received number %d from process 0\n", processId, recievedData);   
+		    if(recievedData==EOS) {
+		    	next = false;
+		    } 
+		    numbersBucketSlave.push_back(recievedData);
+		}	
+		cout << "Displayed vector:\n";
+		display_vector(numbersBucketSlave);
+		cout << "I am done\n";
+		sort(numbersBucketSlave.begin(), numbersBucketSlave.end());
+		cout << "Displayed vector sorted:\n";
+		display_vector(numbersBucketSlave);
+		cout << "I am done sorted\n";		
         // int recievedData;
         // MPI_Recv(&numbers, MSG_SIZE, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
         // cout << "Data:" << numbers;
